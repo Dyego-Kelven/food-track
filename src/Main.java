258e9fa51f205a17001676055408b55773a73bf4
Dyego Kelven <dkelvens@hotmail.com>
2024-12-01 21:52:09 -0300

    ESTQOUE FINALIZADO

--- src/Main.java
+++ src/Main.java
@@ -13,12 +13,12 @@ public class Main {
         Transacao venda = new Venda();
         Produto produto = new Produto();
         ItemPedido itemPedido = new ItemPedido();
-        Estoque estoque = new Estoque("coxinha", 30, 10);
+        Estoque estoque = new Estoque();
         Fidelidade fidelidade = new Fidelidade();
         ClienteService cs = new ClienteService();
         PedidoService ps = new PedidoService();
         VendaService vs = new VendaService();
-        EstoqueService es = new EstoqueService();
+        EstoqueService es = new EstoqueService(estoque);
         FidelidadeService fs = new FidelidadeService();
 
         System.out.println("------------- Sistema de gereciamento de vendas -------------");
@@ -28,31 +28,76 @@ public class Main {
             System.out.println("1 - Menu de Produto\n2 - Atualizar Estoque\n3 - Fazer Pedido\n4 - Consultar Pedidos\n5 - Finalizar Venda\n6 - Gerenciar Fidelidade\n7 - Relatório de Vendas");
             x = sc.nextInt();
 
-            switch (x){
+            switch (x) {
                 case 1:
-                    int d,k;
+                    int d, k;
                     do {
-                        System.out.println("1 - Cadastrar produto\n2 - Atualizar produto\n3 - Ver estoque\n4 - Apagar produto\5 - Ver produtos acabando");
+                        System.out.println("1 - Cadastrar Produto\n2 - Atualizar Quantidade de Produto\n3 - Ver Estoque\n4 - Apagar Produto\n5 - Ver Produtos Abaixo do Estoque Mínimo");
                         d = sc.nextInt();
-                        switch (d){
+                        switch (d) {
                             case 1:
+                                System.out.println("Digite o nome do produto:");
+                                String produtoCadastrar = sc.next();
+                                System.out.println("Digite a quantidade inicial:");
+                                int quantidadeInicial = sc.nextInt();
+                                System.out.println("Digite a quantidade mínima:");
+                                int quantidadeMinima = sc.nextInt();
+                                es.adicionarIngrediente(produtoCadastrar, quantidadeInicial, quantidadeMinima);
                                 break;
+
                             case 2:
+                                System.out.println("Digite o nome do produto para atualizar:");
+                                String produtoAtualizar = sc.next();
+                                System.out.println("Digite a nova quantidade:");
+                                int novaQuantidade = sc.nextInt();
+                                es.atualizarQuantidade(produtoAtualizar, novaQuantidade);
                                 break;
+
                             case 3:
+                                es.exibirStatusEstoque();
                                 break;
+
                             case 4:
+                                System.out.println("Digite o nome do produto para remover:");
+                                String produtoRemover = sc.next();
+                                if (estoque.getEstoqueAtual().containsKey(produtoRemover)) {
+                                    estoque.getEstoqueAtual().remove(produtoRemover);
+                                    estoque.getEstoqueMinimo().remove(produtoRemover);
+                                    System.out.println("Produto " + produtoRemover + " removido com sucesso.");
+                                } else {
+                                    System.out.println("Produto não encontrado no estoque.");
+                                }
                                 break;
+
+                            case 5:
+                                System.out.println("\n--- Produtos Abaixo do Estoque Mínimo ---");
+                                for (String item : estoque.getEstoqueAtual().keySet()) {
+                                    int quantidadeAtual = estoque.getEstoqueAtual().get(item);
+                                    int quantidadeMin = estoque.getEstoqueMinimo().get(item);
+                                    if (quantidadeAtual < quantidadeMin) {
+                                        System.out.println("Produto: " + item);
+                                        System.out.println("Quantidade Atual: " + quantidadeAtual);
+                                        System.out.println("Quantidade Mínima: " + quantidadeMin);
+                                    }
+                                }
+                                break;
+
                             default:
-                                System.out.println("Opção inválida!");
+                                System.out.println("Opção inválida! Tente novamente.");
                         }
-                        System.out.println("Digite 0 para ver o menu novamente");
+                        System.out.println("\nDigite 0 para exibir o menu novamente ou qualquer outro número para voltar ao menu principal.");
                         k = sc.nextInt();
-                    }while (k == 0);
+                    } while (k == 0);
                     break;
+
                 case 2:
-                    //Atualizar Estoque
+                    System.out.println("Digite o nome do produto para atualizar:");
+                    String produtoAtualizar = sc.next();
+                    System.out.println("Digite a nova quantidade:");
+                    int novaQuantidade = sc.nextInt();
+                    es.atualizarQuantidade(produtoAtualizar, novaQuantidade);
                     break;
+
                 case 3:
                     Pessoa funcionario = new Funcionario();
                     Pessoa cliente = new Cliente();
